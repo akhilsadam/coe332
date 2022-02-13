6dc98a464859966c2ab74be2fd58f9835950088a
Akhil Sadam <sadam.akhil@gmail.com>
2022-02-12 18:25:39 -0600

    fix read

--- homework03/water.py
+++ homework03/water.py
@@ -5,7 +5,9 @@ import os
 import json as js
 import numpy as np
 
-datafile="turbidity_data.json"
+name = "turbidity_data"
+ext = ".json"
+datafile=name+ext
 sep=" "
 
 def read(url:str)->dict:
@@ -25,9 +27,33 @@ def read(url:str)->dict:
     
     return data
 
+def turbidity(a0:float,I90:float)->float:
+    """Calculates turbidity.
+
+    Args:
+        a0 (float): calibration constant.
+        I90 (float): 90-degree current
+
+    Returns:
+        float: turbidity in NTU units
+    """
+    return a0*I90
+
+def time(T:float,Ts:float,d:float)->float:
+    """Calculates time to safe water.
+
+    Args:
+        T (float): current turbidity.
+        Ts (float): threshold turbidity.
+        d (float): decay constant.
+
+    Returns:
+        float: time to safe water.
+    """
+    return np.log(Ts/T) / np.log(1-d)
 
 def core():
-    data = read("https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json")
+    data = sorted(read("https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json")[name].items(), key=lambda p: p["datetime"], reverse=True)
 
 if __name__ == '__main__':
     core()

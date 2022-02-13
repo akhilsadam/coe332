01edd4c5373f05300768edbc0a1e6dd3b68bf112
Akhil Sadam <sadam.akhil@gmail.com>
2022-02-13 00:11:07 -0600

    fix read

--- homework03/test_water.py
+++ homework03/test_water.py
@@ -5,13 +5,13 @@ class tests:
     def test_turbid():
         assert water.turbid(3,4) == 12
         assert water.turbid(3,0) == -1
-        # assert type(water.turbid(3,4)) == float
-        # assert type(water.turbid(3,0)) == float
+        assert type(water.turbid(3,4)) == float
+        assert type(water.turbid(3,0)) == float
         assert water.turbid(-3,4) == -1
 
     def test_time():
         assert water.time(1,1) == 0.0
-        # assert type(water.time(1,0.5)) == float
+        assert type(water.time(1,0.5)) == float
         assert abs(water.time(40) - 182.5) < 0.5
         assert water.time(1,1,2.0) == -1
         assert water.time(-1) == -1

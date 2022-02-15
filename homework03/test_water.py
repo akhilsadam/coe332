30ec7011b5cdcaf4c541a89a0ad707cd0a9ca1cb
Akhil Sadam <sadam.akhil@gmail.com>
2022-02-15 12:09:00 -0600

    add tests

--- homework03/test_water.py
+++ homework03/test_water.py
@@ -1,101 +1,121 @@
+from typing import Type
 import water   
 import numpy as np
-class tests:
+import pytest
+# class tests:
 
-    def test_turbid():
-        assert water.turbid(3,4) == 12
-        assert water.turbid(3,0) == 0
-        assert type(water.turbid(3,4)) == int
-        assert type(water.turbid(3.1,0)) == float
-        assert water.turbid(3.1,4) == 12.4
+def test_turbid():
+    assert water.turbid(3,4) == 12
+    assert water.turbid(3,0) == 0
+    assert type(water.turbid(3,4)) == int
+    assert type(water.turbid(3.1,0)) == float
+    assert water.turbid(3.1,4) == 12.4
+    with pytest.raises(TypeError):
+        assert water.turbid(3,"somehow")
 
-    def test_time():
-        assert water.time(1,1) == 0.0
-        assert type(water.time(1,0.5)) == np.float64
-        assert abs(water.time(40) - 182.5) < 0.5
-        assert water.time(1,1,2.0) == -1
-        assert water.time(-1) == -1
+def test_time():
+    assert water.time(1,1) == 0.0
+    assert type(water.time(1,0.5)) == np.float64
+    assert abs(water.time(40) - 182.5) < 0.5
+    assert water.time(1,1,2.0) == -1
+    assert water.time(-1) == -1
+    with pytest.raises(TypeError):
+        assert water.time(np.abs,3)
+    with pytest.raises(TypeError):
+        assert water.time(3,'a')
 
-    def test_sort():
-        assert type(water.sort([
-            {"a":5,"b":6},
-            {"a":4,"b":8},
-            {"a":5,"b":7}
-        ],"b")) == list
-        assert water.sort([],"b") == []
+def test_sort():
+    assert type(water.sort([
+        {"a":5,"b":6},
+        {"a":4,"b":8},
+        {"a":5,"b":7}
+    ],"b")) == list
+    assert water.sort([],"b") == []
+    assert water.sort([
+        {"a":5},
+        {"a":4},
+        {"a":5}
+    ],"a") == [
+        {"a":5},
+        {"a":5},
+        {"a":4}
+    ]
+    assert water.sort([
+        {"a":4,"b":8},
+    ],"b") == [
+        {"a":4,"b":8},
+    ]
+    assert water.sort([
+        {"a":5,"b":6},
+        {"a":4,"b":8},
+        {"a":5,"b":7}
+    ],"b") == [
+        {"a":4,"b":8},
+        {"a":5,"b":7},
+        {"a":5,"b":6}
+    ]
+    with pytest.raises(TypeError):
+        assert water.sort(3)
+    with pytest.raises(TypeError):
         assert water.sort([
-            {"a":5},
-            {"a":4},
-            {"a":5}
-        ],"a") == [
-            {"a":5},
-            {"a":5},
-            {"a":4}
-        ]
-        assert water.sort([
-            {"a":4,"b":8},
-        ],"b") == [
-            {"a":4,"b":8},
-        ]
-        assert water.sort([
-            {"a":5,"b":6},
-            {"a":4,"b":8},
-            {"a":5,"b":7}
-        ],"b") == [
-            {"a":4,"b":8},
-            {"a":5,"b":7},
-            {"a":5,"b":6}
-        ]
+        {"a":5,"b":6},
+        {"a":4,"b":8},
+        {"a":5,"b":7}
+    ],"b") == [
+        {"a":4,"b":8},
+        {"a":5,"b":7},
+        {"a":5,"b":'s'}
+    ]
 
-    def test_current():
-        assert type(water.current([
-            {water.cc:5,water.dc:6},
-        ])) == float
-        assert water.current([
-            {water.cc:5,water.dc:6},
-        ]) == 30
-        assert water.current([
-            {water.cc:5,water.dc:6},
-            {water.cc:5,water.dc:0},
-            {water.cc:4,water.dc:6},
-            {water.cc:5,water.dc:0},
-            {water.cc:5,water.dc:6},
-        ]) == 84/5
-        assert water.current([
-            {water.cc:5,water.dc:6},
-            {water.cc:5,water.dc:0},
-            {water.cc:4,water.dc:6},
-            {water.cc:5,water.dc:0},
-            {water.cc:5,water.dc:6},
-            {water.cc:5,water.dc:6},
-            {water.cc:5,water.dc:6},
-            {water.cc:5,water.dc:6},
-            {water.cc:5,water.dc:6},
-            {water.cc:5,water.dc:6},
-        ]) == 84/5
-        assert water.current([
-            {water.cc:5,water.dc:6},
-            {water.cc:5,water.dc:0},
-            {water.cc:4,water.dc:6},
-            {water.cc:5,water.dc:0},
-            {water.cc:5,water.dc:6},
-        ],1) == 30
-       
-    def test_read():
-        assert type(water.read("https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json"))==dict
-        assert type(water.read("https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json")[water.name])==list
-        assert type(water.read("---"))==dict #since we now have the data downloaded
-        assert water.read("https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json")[water.name][0][water.cc] == 1.022
-        assert water.read("https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json")[water.name][5][water.dc] == 1.104
-        
+def test_current():
+    assert type(water.current([
+        {water.cc:5,water.dc:6},
+    ])) == float
+    assert water.current([
+        {water.cc:5,water.dc:6},
+    ]) == 30
+    assert water.current([
+        {water.cc:5,water.dc:6},
+        {water.cc:5,water.dc:0},
+        {water.cc:4,water.dc:6},
+        {water.cc:5,water.dc:0},
+        {water.cc:5,water.dc:6},
+    ]) == 84/5
+    assert water.current([
+        {water.cc:5,water.dc:6},
+        {water.cc:5,water.dc:0},
+        {water.cc:4,water.dc:6},
+        {water.cc:5,water.dc:0},
+        {water.cc:5,water.dc:6},
+        {water.cc:5,water.dc:6},
+        {water.cc:5,water.dc:6},
+        {water.cc:5,water.dc:6},
+        {water.cc:5,water.dc:6},
+        {water.cc:5,water.dc:6},
+    ]) == 84/5
+    assert water.current([
+        {water.cc:5,water.dc:6},
+        {water.cc:5,water.dc:0},
+        {water.cc:4,water.dc:6},
+        {water.cc:5,water.dc:0},
+        {water.cc:5,water.dc:6},
+    ],1) == 30
+    
+def test_read():
+    assert type(water.read("https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json"))==dict
+    assert type(water.read("https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json")[water.name])==list
+    assert type(water.read("---"))==dict #since we now have the data downloaded
+    assert water.read("https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json")[water.name][0][water.cc] == 1.022
+    assert water.read("https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json")[water.name][5][water.dc] == 1.104
+    
 
-def run():
-    "Run all tests."
-    for test in dir(tests):
-        item = getattr(tests,test)
-        if callable(item) and test.startswith('test'):
-            print(item)
-            item()
+# def run():
+#     "Run all tests."
+#     for test in dir(tests):
+#         item = getattr(tests,test)
+#         if callable(item) and test.startswith('test'):
+#             print(item)
+#             item()
 
-if __name__ == '__main__':
-    run()
+# if __name__ == '__main__':
+#     run()

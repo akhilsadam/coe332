05ce3c9d8e913f6e7d1285735628b4f74ea9420d
Akhil Sadam <sadam.akhil@gmail.com>
2022-04-17 19:21:38 -0500

    update

--- homework06/repl.sh
+++ homework06/repl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 firstString=$(<deployment.yml)
-secondString=$(kubectl get services)
+secondString=$(kubectl get services flask-cube-redis-redis-service --output=jsonpath="{.spec.clusterIP}")
 echo $secondString
 echo "${firstString/ip_address/"$secondString"}"   
